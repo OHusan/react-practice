b030fcd605115d86424c2a9b72361b64f998b781
OHusan <omerinovic27@gmail.com>
2024-01-08 16:43:27 +0100

    investment page

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(newValue, inputIdentifier) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <UserInput
+        onChange={handleChange}
+        userInput={userInput}
+      />
+      {!inputIsValid && (
+        <p className="center">Please enter value greater then 0</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
+    </>
+  );
+}
+
+export default App;
